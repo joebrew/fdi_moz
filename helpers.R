e9ca8c22c6ceab2629e37d701ba4c80ce4fb7f65
joebrew <joebrew@gmail.com>
2015-09-25 10:25:45 -0400

    clean up and maps

---
+++ helpers.R
@@ -0,0 +1,122 @@
+
+##### SHARED LEGEND FOR MULTIPLE PLOTS GGPLOT2
+# https://github.com/hadley/ggplot2/wiki/Share-a-legend-between-two-ggplot2-graphs
+grid_arrange_shared_legend <- function(...) {
+  plots <- list(...)
+  g <- ggplotGrob(plots[[1]] + theme(legend.position="bottom"))$grobs
+  legend <- g[[which(sapply(g, function(x) x$name) == "guide-box")]]
+  lheight <- sum(legend$height)
+  grid.arrange(
+    do.call(arrangeGrob, lapply(plots, function(x)
+      x + theme(legend.position="none"))),
+    legend,
+    ncol = 1,
+    heights = unit.c(unit(1, "npc") - lheight, lheight))
+}
+
+#####
+# EXTRACT JUST THE LEGEND
+# http://stackoverflow.com/questions/12539348/ggplot-separate-legend-and-plot
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  legend
+}
+
+##### CONFIDENCE INTERVALS ON PROPORTIONS
+# (https://aghaynes.wordpress.com/2014/04/09/calculating-confidence-intervals-for-proportions/)
+simpasym <- function(n, p, z=1.96, cc=TRUE){
+  out <- list()
+  if(cc){
+    out$lb <- p - z*sqrt((p*(1-p))/n) - 0.5/n
+    out$ub <- p + z*sqrt((p*(1-p))/n) + 0.5/n
+  } else {
+    out$lb <- p - z*sqrt((p*(1-p))/n)
+    out$ub <- p + z*sqrt((p*(1-p))/n)
+  }
+  out
+}
+
+
+capwords <- function(s, strict = FALSE) {
+  cap <- function(s) paste(toupper(substring(s, 1, 1)),
+                           {s <- substring(s, 2); if(strict) tolower(s) else s},
+                           sep = "", collapse = " " )
+  sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
+}
+
+library(reshape2)
+
+multiplot <- function(..., plotlist = NULL, file, cols = 1, layout = NULL) {
+  require(grid)
+  
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  if (is.null(layout)) {
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots == 1) {
+    print(plots[[1]])
+    
+  } else {
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    for (i in 1:numPlots) {
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+#####
+# KEEP ONLY N LEVELS OF A CATEGORICAL VARIABLE
+# (AND ASSIGN ALL OTHER LEVELS TO SOMETHING ELSE)
+#####
+simplify <- function(var, n = 10, other = NA, empty_as_other = FALSE){
+  var <- as.character(var)
+  if(empty_as_other){
+    var[which(var == '')] <- other
+  }
+  x <- as.data.frame(table(var)); x <- x[rev(order(x$Freq)),]
+  keeps <- x$var[1:n]
+  var <- ifelse(var %in% keeps, var, other)
+  var <- factor(var)
+  return(var)
+}
+
+
+# COLLAPSE SHAPEFILE INTO ONE POLYGON
+collapse_map <- function(x){
+  boundary <- unionSpatialPolygons(x, rep(1, length(x@polygons)))
+  return(boundary)
+}
+
+
+#####
+# GET EUCLIDEAN DISTANCE IN KM
+#####
+get_distance <- function(lon1, 
+                         lat1, 
+                         lon2, 
+                         lat2){
+  rad <- pi/180
+  a1 <- lat1 * rad
+  a2 <- lon1 * rad
+  b1 <- lat2 * rad
+  b2 <- lon2 * rad
+  dlon <- b2 - a2
+  dlat <- b1 - a1
+  a <- (sin(dlat/2))^2 + cos(a1) * cos(b1) * (sin(dlon/2))^2
+  c <- 2 * atan2(sqrt(a), sqrt(1 - a))
+  R <- 6378.145
+  d <- R * c
+  return(d)
+}
